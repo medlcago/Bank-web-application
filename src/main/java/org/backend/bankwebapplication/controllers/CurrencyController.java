7b052d6f47ae443be77f5b0e93bf621bc62b52bb
medlcago <sasha.vol40@mail.ru>
2024-03-11 17:29:36 +0300

    Добавлена страница с курсами валют

---
+++ src/main/java/org/backend/bankwebapplication/controllers/CurrencyController.java
@@ -0,0 +1,22 @@
+package org.backend.bankwebapplication.controllers;
+
+import org.backend.bankwebapplication.services.CurrencyService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CurrencyController {
+    CurrencyService currencyService;
+
+    public CurrencyController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    @GetMapping(value = "/currency")
+    public String getCurrency(Model model) {
+        model.addAttribute("title", "Курс валют");
+        model.addAttribute("valutes", currencyService.getDailyCurs().getValutes());
+        return "currency";
+    }
+}
