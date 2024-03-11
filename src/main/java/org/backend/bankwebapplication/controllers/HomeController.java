afe01891a1cadd1c99211479d772b01363fdcb38
medlcago <sasha.vol40@mail.ru>
2024-03-11 13:53:03 +0300

    Добавлен loading_overlay

--- src/main/java/org/backend/bankwebapplication/controllers/HomeController.java
+++ src/main/java/org/backend/bankwebapplication/controllers/HomeController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
-
 @Controller
 public class HomeController {
     @GetMapping(value = "/home")
-    public String homePage(Model model, Principal principal) {
+    public String homePage(Model model) {
         model.addAttribute("title", "Главная страница");
         return "home";
     }
