b6b8fe65f26a9d2accb66f71f52041988f010a7d
medlcago <sasha.vol40@mail.ru>
2024-03-13 16:24:11 +0300

    Валидация на стороне сервера

--- src/main/java/org/backend/bankwebapplication/dto/UserRegistrationForm.java
+++ src/main/java/org/backend/bankwebapplication/dto/UserRegistrationForm.java
@@ -1,5 +1,7 @@
 package org.backend.bankwebapplication.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegistrationForm {
+    @Size(min = 6, max = 16, message = "Имя должно содержать от 6 до 16 символов")
     private String username;
+
+    @Email(message = "Электронная почта должна быть действующей")
     private String email;
+
+    @Size(min = 6, max = 32, message = "Пароль должен содержать от 6 до 32 символов")
     private String password;
+
+    @Size(min = 6, max = 32, message = "Пароль должен содержать от 6 до 32 символов")
     private String confirmPassword;
 }
