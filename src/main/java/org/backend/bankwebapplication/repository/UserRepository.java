bde439a93ab347b8e8297c17953fce5c46082680
medlcago <sasha.vol40@mail.ru>
2024-03-19 02:11:52 +0300

    Добавлена возможность логина по email

--- src/main/java/org/backend/bankwebapplication/repository/UserRepository.java
+++ src/main/java/org/backend/bankwebapplication/repository/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, BigInteger> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
 }
