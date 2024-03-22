8c02123f3bda6e5a1a7e75cec8fba028debd5431
medlcago <sasha.vol40@mail.ru>
2024-03-22 17:40:55 +0300

    EmailService: Добавлен метод для отправки сообщений

--- src/main/java/org/backend/bankwebapplication/services/EmailService.java
+++ src/main/java/org/backend/bankwebapplication/services/EmailService.java
@@ -1,7 +1,10 @@
 package org.backend.bankwebapplication.services;
 
+import jakarta.mail.MessagingException;
 import org.backend.bankwebapplication.dto.email.EmailCleanResponse;
 
 public interface EmailService {
     EmailCleanResponse cleanEmail(String email);
+
+    void sendEmail(String to, String subject, String message, boolean isHtml) throws MessagingException;
 }
--- src/main/java/org/backend/bankwebapplication/services/impl/EmailServiceImpl.java
+++ src/main/java/org/backend/bankwebapplication/services/impl/EmailServiceImpl.java
@@ -1,18 +1,27 @@
 package org.backend.bankwebapplication.services.impl;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.backend.bankwebapplication.dto.email.EmailCleanResponse;
 import org.backend.bankwebapplication.services.EmailService;
 import org.springframework.core.env.Environment;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmailServiceImpl implements EmailService {
     private final Environment environment;
+    private final JavaMailSender mailSender;
 
-    public EmailServiceImpl(Environment environment) {
+    public EmailServiceImpl(Environment environment, JavaMailSender mailSender) {
         this.environment = environment;
+        this.mailSender = mailSender;
     }
 
     @Override
@@ -37,4 +46,22 @@ public class EmailServiceImpl implements EmailService {
         }
         return null;
     }
+
+    @Override
+    public void sendEmail(String to, String subject, String message, boolean isHtml) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        if (isHtml) {
+            mimeMessage.setContent(message, "text/html; charset=utf-8");
+        } else {
+            mimeMessage.setText(message, "utf-8");
+        }
+
+        mailSender.send(mimeMessage);
+    }
 }
+
