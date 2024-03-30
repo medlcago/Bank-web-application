c81e01aa0d99f86280c39055dd89841a3797be5a
medlcago <sasha.vol40@mail.ru>
2024-03-30 14:29:53 +0300

    Добавлена страница 404

--- src/main/java/org/backend/bankwebapplication/controllers/AdviceController.java
+++ src/main/java/org/backend/bankwebapplication/controllers/AdviceController.java
@@ -1,13 +1,17 @@
 package org.backend.bankwebapplication.controllers;
 
 import org.backend.bankwebapplication.security.user.UserDetailsImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
-public class GlobalController {
-
+public class AdviceController {
     @ModelAttribute("authenticated")
     public boolean isAuthenticated(Authentication authentication) {
         return authentication != null && authentication.isAuthenticated();
@@ -22,5 +26,11 @@ public class GlobalController {
             }
         }
         return null;
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView pageNotFound() {
+        return new ModelAndView("errors/404");
     }
 }
